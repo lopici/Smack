2ab91d757d039615b8766e490be943d1c78abaca
Andras <andras@MacBook.local>
2017-10-29 00:06:12 +1100

    lesson 11.

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -34,7 +34,11 @@ class CreateAccountVC: UIViewController {
         print(pass)
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if (success) {
-                print("registered user")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("logged in useer!", AuthService.instance.authToken)
+                    }
+                })
             }
         }
         
--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import  Alamofire
+import SwiftyJSON
 
 class AuthService { //singelton - only one instance will exist
     
@@ -46,15 +47,13 @@ class AuthService { //singelton - only one instance will exist
         
         let lowerCaseEmail = email.lowercased()
         
-        let header = [
-            "Content-Type": "application/json; charset=utf-8"
-        ]
+        
         let body: [String : Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             if response.result.error == nil {
                 completion(true)
                 
@@ -68,4 +67,40 @@ class AuthService { //singelton - only one instance will exist
     }
     
     
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+       
+        let body: [String : Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                //Swift way of parsing json
+//                if let json = response.result.value as? Dictionary<String, Any>{
+//                    if let email = json["user"] as? String {
+//                        self.userEmail = email
+//                    }
+//                    if let token = json["token"] as? String {
+//                        self.authToken = token
+//                    }
+//            }
+                //Using SwiftyJSON
+                guard let data = response.data else {return}
+                let json = JSON(data: data)
+                self.userEmail = json["user"].stringValue //this safely unwraps the value
+                self.authToken = json["token"].stringValue
+       
+                self.isLoggedIn = true
+                
+                completion(true)
+            }
+            else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
 }
