3488869709c996c85b14204221c21172467e3819
Andras <andras@MacBook.local>
2017-10-31 22:03:35 +1100

    Lesson 16.

--- Smack/Services/UserDataService.swift
+++ Smack/Services/UserDataService.swift
@@ -28,4 +28,38 @@ class UserDataService {
     func setAvatarName(avatarName: String) {
         self.avatarName = avatarName
     }
+    
+    func returnUIColor(components: String) -> UIColor {
+        let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a :NSString?
+        
+        scanner.scanUpToCharacters(from: comma, into: &r)//scanning up to the first comma and saving it into r
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        //if any unwraps fail, return a default color
+        guard let rUnwrapped = r else { return defaultColor}
+        guard let gUnwrapped = g else { return defaultColor}
+        guard let bUnwrapped = b else { return defaultColor}
+        guard let aUnwrapped = a else { return defaultColor}
+        
+        let rfloat = CGFloat(rUnwrapped.doubleValue)
+        let gfloat = CGFloat(gUnwrapped.doubleValue)
+        let bfloat = CGFloat(bUnwrapped.doubleValue)
+        let afloat = CGFloat(aUnwrapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rfloat, green: gfloat, blue: bfloat, alpha: afloat)
+        
+        
+        return newUIColor
+    }
+    
+    
+    
 }
